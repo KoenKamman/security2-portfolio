cdf7b9c513e10fc1e0c9f306bcc9e4fc38a6d4c3
Koen Kamman <kammankoen@gmail.com>
2019-02-24 14:15:05 +0100

    assignment2

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -11,6 +11,69 @@ export class AppComponent implements OnInit {
   title = "security2-portfolio";
   sidenavOpen = true;
 
+  displayedColumns: string[] = [
+    "threat",
+    "title",
+    "description",
+    "location",
+    "mitigation"
+  ];
+  assignment2TableDataSource = [
+    {
+      threat: "Spoofing",
+      title: "Spoofing the Client (Phone) External Entity",
+      description:
+        "Client (Phone) may be spoofed by an attacker and this may lead to unauthorized access to Google Drive Backup Processing. Consider using a standard authentication mechanism to identify the external entity.",
+      location: "Client (Phone) --> Google Drive Backup Processing",
+      mitigation: "Authenticatie procedure tussen gebruiker en server."
+    },
+    {
+      threat: "Tampering",
+      title: "Persistent Cross Site Scripting",
+      description:
+        "The web server 'Google Drive Backup Processing' could be a subject to a persistent cross-site scripting attack because it does not sanitize data store 'Google Cloud Storage' inputs and output.",
+      location: "Google Drive Backup Processing --> Google Cloud Storage",
+      mitigation: "Filters toepassen om code uit data input te verwijderen."
+    },
+    {
+      threat: "Repudiation",
+      title: "Non-repudiation sending messages.",
+      description:
+        "Client (Phone) could deny having sent a certain message due to poor authentication or insecuring logging.",
+      location: "Client (Phone) --> Message processing",
+      mitigation:
+        "Securing logging with encryption. Stronger authentication and signing messages with encryption keys."
+    },
+    {
+      threat: "Information Disclosure",
+      title: "Weak Access Control for a Resource",
+      description:
+        "Improper data protection of Cache can allow an attacker to read information not intended for disclosure. Review authorization settings.",
+      location: "WhatsApp Server --> Cache",
+      mitigation:
+        "Data encrypten voordat het verzonden en opgeslagen wordt in een data store."
+    },
+    {
+      threat: "Denial Of Service",
+      title:
+        "Potential Excessive Resource Consumption for WhatsApp Server or Cache",
+      description:
+        "Does WhatsApp Server or Cache take explicit steps to control resource consumption? Resource consumption attacks can be hard to deal with, and there are times that it makes sense to let the OS do the job. Be careful that your resource requests don't deadlock, and that they do timeout.",
+      location: "WhatsApp Server --> Cache",
+      mitigation:
+        "Filteren van recurring requests, quotas zetten op het aantal requests per gebruiker (per interval)."
+    },
+    {
+      threat: "Elevation Of Privilege",
+      title: "Elevation Using Impersonation",
+      description:
+        "Client (Webbrowser) may be able to impersonate the context of Message processing in order to gain additional privilege.",
+      location: "Message Processing --> Client (Webbrowser)",
+      mitigation:
+        "Access Control Lists om alleen geverifieerde apparaten bepaalde acties te laten uitvoeren."
+    }
+  ];
+
   constructor(
     private matIconRegistry: MatIconRegistry,
     private domSanitizer: DomSanitizer,
--- portfolio/src/app/modules/material/material.module.ts
+++ portfolio/src/app/modules/material/material.module.ts
@@ -5,7 +5,8 @@ import {
   MatIconModule,
   MatCardModule,
   MatSidenavModule,
-  MatListModule
+  MatListModule,
+  MatTableModule
 } from "@angular/material";
 
 @NgModule({
@@ -15,7 +16,8 @@ import {
     MatIconModule,
     MatCardModule,
     MatSidenavModule,
-    MatListModule
+    MatListModule,
+    MatTableModule
   ],
   exports: [
     MatToolbarModule,
@@ -23,7 +25,8 @@ import {
     MatIconModule,
     MatCardModule,
     MatSidenavModule,
-    MatListModule
+    MatListModule,
+    MatTableModule
   ]
 })
 export class MaterialModule {}
